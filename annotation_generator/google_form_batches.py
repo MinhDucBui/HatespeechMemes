2576a6393b4e6a979f40f6f3360d49f3e7022413
MinhDucBui <minhducbui1996@yahoo.de>
2024-08-07 16:23:12 +0200

    before batch size

--- annotation_generator/google_form_batches.py
+++ annotation_generator/google_form_batches.py
@@ -3,6 +3,8 @@ import os
 import random
 import shutil
 import pandas as pd
+from typing import List
+
 
 BATCH_SIZE = 36
 PAGE_SIZE = 6
@@ -36,71 +38,51 @@ def pop_elements_by_range(my_list, start_index, end_index):
     return popped_elements
 
 
-# Hardcoded: Because we have 45 groups, 8 instances. Distribute over 10 batches.
-def distribute_instances(all_groups):
-    # Number of batches
-    num_batches = 10
-
-    # Initialize batches
+def redistribute_elements(groups, num_batches):
+    # Initialize batches as empty lists
     batches = [[] for _ in range(num_batches)]
+    current_batch = 0
 
-    # Distribute instances to batches
-    for group in all_groups:
-        for idx, instance in enumerate(group):
-            # Determine which batch to put this instance in
-            batch_number = (idx % num_batches)
-            batches[batch_number].append(instance)
+    # Distribute elements in a round-robin fashion
+    for group in groups:
+        for element in group:
+            batches[current_batch].append(element)
+            current_batch = (current_batch + 1) % num_batches
 
-    for batch_number in range(8, 10):
-        instance_range = 0
-        if batch_number == 8:
-            for group_index in range(4):
-                elements = pop_elements_by_range(
-                    batches[group_index], instance_range*9, (instance_range*9+9))
-                batches[batch_number] += elements
-                instance_range += 1
-        elif batch_number == 9:
-            for group_index in range(4, 8):
-                elements = pop_elements_by_range(
-                    batches[group_index], instance_range*9, (instance_range*9+9))
-                batches[batch_number] += elements
-                instance_range += 1
-    all_a = []
     for i, batch in enumerate(batches):
-        all_a += batch
-        assert len(
-            batch) == 36, f"Batch {i} has {len(batch)} instances instead of 36."
+        print("Length of batch {}: {}".format(i, len(batch)))
+
     return batches
+
 
 def restructure_list_of_lists(lists):
     # Flatten the list of lists into a single list
     flat_list = [item for sublist in lists for item in sublist]
-    
+
     # Split the flattened list into chunks of 36 elements
-    restructured_list = [flat_list[i:i + 36] for i in range(0, len(flat_list), 36)]
-    
+    restructured_list = [flat_list[i:i + 27]
+                         for i in range(0, len(flat_list), 27)]
+
     return restructured_list
 
 
 def filter_batch(all_groups, filter_folder):
-    if filter_folder != "":
-        df_filtered = []
-        for language in LANGUAGE:
-            filter_language = os.path.join(
-                filter_folder, "PRELIM_" + language + ".csv")
+    df_filtered = []
+    for language in LANGUAGE:
+        filter_language = os.path.join(
+            filter_folder + language + ".csv")
+        if os.path.isfile(filter_language):
             df_filtered.append(pd.read_csv(filter_language))
-        df_filtered = pd.concat(df_filtered, ignore_index=True)
-        image_ids = list(df_filtered["Image ID"].astype(str))
+    df_filtered = pd.concat(df_filtered, ignore_index=True)
+    image_ids = list(df_filtered["Image ID"].astype(str))
 
     new_all_groups = []
     for index_batch, group in enumerate(all_groups):
         new_all_groups.append([])
         for instance in group:
-            if filter_folder != "":
-                instance_check = instance.split("/")[-1].split(".jpg")[0]
-                if instance_check in image_ids:
-                    new_all_groups[index_batch].append(instance)
-
+            instance_check = instance.split("/")[-1].split(".jpg")[0]
+            if instance_check in image_ids:
+                new_all_groups[index_batch].append(instance)
     new_all_groups = restructure_list_of_lists(new_all_groups)
 
     return new_all_groups
@@ -111,9 +93,9 @@ if __name__ == '__main__':
     parser.add_argument('--hatespeech', '-s', type=str,
                         default='/Users/duc/Desktop/Projects/Ongoing/MultiModalMemes/dataset/annotation/hatespeech_nonhate/images')
     parser.add_argument('--output', '-o', type=str,
-                        default='/Users/duc/Desktop/Projects/Ongoing/MultiModalMemes/dataset/annotation/google_form_nonhate')
+                        default='/Users/duc/Desktop/Projects/Ongoing/MultiModalMemes/dataset/annotation/google_form_nonhate_filter')
     parser.add_argument('--filter', '-f', type=str,
-                        default='/Users/duc/Desktop/Projects/Ongoing/MultiModalMemes/annotation_evaluation/data/filter/data')
+                        default='/Users/duc/Desktop/Projects/Ongoing/MultiModalMemes/annotation_evaluation/data/filter/data/NewPrelim')
 
     args = parser.parse_args()
 
@@ -136,11 +118,12 @@ if __name__ == '__main__':
             all_groups.append(instances)
 
     # Distribute the instances
-    all_groups = distribute_instances(all_groups)
+    all_groups = redistribute_elements(all_groups, 15)
 
     if filter_folder != "":
         all_groups = filter_batch(all_groups, filter_folder)
 
+    max_page = 4
     for language in LANGUAGE:
         page_index = 0
         for index_batch, group in enumerate(all_groups):
@@ -156,6 +139,4 @@ if __name__ == '__main__':
                     target_file = os.path.join(batch_folder, filename)
                     # copy2 preserves metadata
                     shutil.copy2(instance, target_file)
-
-                    if (instance_index+1) % 6 == 0:
-                        page_index += 1
+                    page_index = (page_index + 1) % max_page
